d1b30883aba392dfb8ef7e9b6e41f6b11f7fc5e2
Harsh Maniyar <harshsmaniyar@gmail.com>
2026-04-06 21:34:10 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,10 +1,10 @@
-correct_username = "Harsh"
-correct_password = "12345678"
+actual_username = "Harsh"
+actual_password = "12345678"
 
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-if username == correct_username and password == correct_password:
+if username == actual_username and password == actual_password:
     print("Login Successful")
 else:
     print("Login Failed")
