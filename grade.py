1b742d1e7df8f311fe438db3231145ef70e7edc8
Harsh Maniyar <harshsmaniyar@gmail.com>
2026-04-06 21:15:21 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,16 +1,24 @@
-marks = int(input("Enter your marks: "))
+m1 = int(input("Enter marks of subject 1: "))
+m2 = int(input("Enter marks of subject 2: "))
+m3 = int(input("Enter marks of subject 3: "))
+m4 = int(input("Enter marks of subject 4: "))
+m5 = int(input("Enter marks of subject 5: "))
 
-if marks < 40:
+total = m1 + m2 + m3 + m4 + m5
+percentage = total / 5
+
+print("Total Marks =", total)
+print("Percentage =", percentage)
+
+if percentage < 40:
     print("Grade: Fail")
-elif 40 <= marks < 50:
+elif 40 <= percentage < 50:
     print("Grade: D")
-elif 50 <= marks < 60:
+elif 50 <= percentage < 60:
     print("Grade: C")
-elif 60 <= marks < 75:
+elif 60 <= percentage < 75:
     print("Grade: B")
-elif 75 <= marks < 90:
+elif 75 <= percentage < 90:
     print("Grade: A")
-elif marks >= 90:
-    print("Grade: O")
 else:
-    print("Invalid Input")
+    print("Grade: O")
